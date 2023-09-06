81f271be93c928ada48dad139c414feba55592f8
MikeTeddyOmondi <mike_omondi@outlook.com>
2023-09-06 21:01:09 +0300

    Updates - interceptors & index.html

--- admin/src/interceptors/axios.js
+++ admin/src/interceptors/axios.js
@@ -3,11 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:8009/api";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] =
   "http://localhost:3000";
-// axios.defaults.headers.post["Access-Control-Allow-Origin"] =
-//   "http://localhost:3000";
-// axios.defaults.headers.post["Origin"] =
-//   "http://localhost:8009";
-// axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 
 let refresh = false;
 
@@ -27,12 +22,7 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
   (resp) => resp,
   async (error) => {
-    console.log({ responseError: error });
-
-    // if (error.code === "ERR_NETWORK") {
-    //   console.log(`AxiosError: ${error.message}`);
-    //   return;
-    // }
+    // console.log({ responseError: error });
 
     if (error.response.status === 401 && !refresh) {
       refresh = true;
